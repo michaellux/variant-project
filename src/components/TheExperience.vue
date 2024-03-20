e61039de1a8c5944767dd6ef2a07dacb5b023b05
Sidachenko Michael <michaellux@yandex.ua>
2024-03-20 19:09:53 +0300

    add start items, resources

--- src/components/TheExperience.vue
+++ src/components/TheExperience.vue
@@ -1,40 +1,36 @@
 <script setup lang="ts">
-import { TresCanvas, useRenderLoop } from '@tresjs/core';
-import { reactive, shallowRef } from 'vue';
-import { BasicShadowMap, SRGBColorSpace, NoToneMapping, Vector3 } from 'three';
-import { OrbitControls } from '@tresjs/cientos';
-import { TresLeches, useControls } from '@tresjs/leches';
-import '@tresjs/leches/styles';
+import { TresCanvas, useRenderLoop } from "@tresjs/core";
+import { reactive, shallowRef } from "vue";
+import { BasicShadowMap, SRGBColorSpace, NoToneMapping, Vector3 } from "three";
+import { OrbitControls } from "@tresjs/cientos";
+import TheControlPanel from "./TheControlPanel.vue";
+import "@tresjs/leches/styles";
 
 const gl = reactive({
-  clearColor: '#82DBC5',
+  clearColor: "#b9b9b4",
   shadows: true,
   alpha: false,
   shadowMapType: BasicShadowMap,
   outputColorSpace: SRGBColorSpace,
   toneMapping: NoToneMapping,
 });
-
-const { value: position } = useControls({
-  position: new Vector3(0, 0, 0),
-});
 </script>
 
 <template>
-  <TresLeches />
+  <TheControlPanel />
   <TresCanvas v-bind="gl">
-    <TresPerspectiveCamera :position="[5, 5, 5]" />
+    <TresPerspectiveCamera
+      :position="[-20, 10, 20]"
+      :fov="75"
+      :near="0.1"
+      :far="1000"
+      look-at="[0, 0, 0]"
+    />
     <OrbitControls />
     <TresAmbientLight :intensity="0.5" :color="'red'" />
-    <!-- this doesn't seem to be working, I must be doing it wrong -->
-    <TresMesh :position="position">
-      <!-- the only way it works locally is -->
-      <!-- <TresMesh :position-x="position.x" :position-y="position.y" :position-z="position.z" -->
-      <TresBoxGeometry :args="[1, 1, 1]" />
-      <TresMeshNormalMaterial />
-    </TresMesh>
+
     <TresDirectionalLight :position="[0, 2, 4]" :intensity="1" cast-shadow />
-    <TresAxesHelper />
-    <TresGridHelper :args="[10, 10, 0x444444, 'teal']" />
+    <TresGridHelper :args="[500, 50]" />
+    <TresAxesHelper :args="[100]" />
   </TresCanvas>
 </template>
---
+++ src/sources.ts
@@ -0,0 +1,125 @@
+import { sub } from './../node_modules/.pnpm/@types+three@0.152.1/node_modules/@types/_tmp_25084_982bcfcc5c1c232fd867f3d41004429f/examples/jsm/nodes/shadernode/ShaderNodeBaseElements.d';
+export default [
+  {
+     name: 'chairModel',
+     type: 'model',
+     path: 'models/chair.glb',
+  },
+  {
+    name: 'cubeModel',
+    type: 'model',
+    path: 'models/cube.glb',
+  },
+  {
+    name: 'helmetModel',
+    type: 'model',
+    path:'models/helmet.glb'
+  },
+  {
+    name: 'suzanneModel',
+    type: 'model', 
+    path:'models/suzanne.glb'
+  },
+  {
+    name: 'albedoLeatherTexture',
+    type: 'texture',
+    subtype: 'albedo',
+    path: 'textures/albedo/albedo-leather.ktx2'
+  },
+  {
+    name: 'albedoMetalTexture',
+    type: 'texture',
+    subtype: 'albedo',
+    path: 'textures/albedo/albedo-metal.png'
+  },
+  {
+    name: 'albedoVeloursTexture',
+    type: 'texture',
+    subtype: 'albedo',
+    path: 'textures/albedo/albedo-velours.png'
+  },
+  {
+    name: 'albedoWoodTexture',
+    type: 'texture',
+    subtype: 'albedo',
+    path: 'textures/albedo/albedo-wood.png'
+  },
+  {
+    name: 'roughnessLeatherTexture',
+    type: 'texture',
+    subtype: 'roughness',
+    path: 'textures/roughness/roughness-leather.ktx2'
+  },
+  {
+    name: 'roughnessMetalTexture',
+    type: 'texture',
+    subtype: 'roughness',
+    path: 'textures/roughness/roughness-metal.png'
+  },  
+  {
+    name: 'roughnessVeloursTexture',
+    type: 'texture',
+    subtype: 'roughness',
+    path: 'textures/roughness/roughness-velours.png'
+  },
+  {
+    name: 'roughnessWoodTexture',
+    type: 'texture',
+    subtype: 'roughness',
+    path: 'textures/roughness/roughness-wood.png'
+  },
+  {
+    name: 'metalnessLeatherTexture',
+    type: 'texture',
+    subtype:'metalness',
+    path: 'textures/metalness/metalness-leather.ktx2'
+  },
+  {
+    name: 'metalnessMetalTexture',
+    type: 'texture',
+    subtype:'metalness',
+    path: 'textures/metalness/metalness-metal.png'
+  },
+  {
+    name: 'metalnessVeloursTexture',
+    type: 'texture',
+    subtype:'metalness',
+    path: 'textures/metalness/metalness-velours.png'
+  },
+  {
+    name: 'metalnessWoodTexture',
+    type: 'texture',
+    subtype:'metalness',
+    path: 'textures/metalness/metalness-wood.png'
+  },
+  {
+    name: 'normalLeatherTexture',
+    type: 'texture',
+    subtype: 'normal',
+    path: 'textures/normal/normal-leather.ktx2'
+  },
+  {
+    name: 'normalMetalTexture',
+    type: 'texture',
+    subtype: 'normal',
+    path: 'textures/normal/normal-metal.png'
+  },
+  {
+    name: 'normalVeloursTexture',
+    type: 'texture',
+    subtype: 'normal',
+    path: 'textures/normal/normal-velours.png'
+  },
+  {
+    name: 'normalWoodTexture',
+    type: 'texture',
+    subtype: 'normal',
+    path: 'textures/normal/normal-wood.png'
+  },
+  {
+    name: 'sheenLeatherTexture',
+    type: 'texture',
+    subtype:'sheen',
+    path: 'textures/sheen/sheen-velours.png'
+  }
+]
