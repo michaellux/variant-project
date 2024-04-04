53e33c6b9ba02b0986e697a28b313da8b0ed661f
Sidachenko Michael <michaellux@yandex.ua>
2024-04-04 23:27:46 +0300

    fix error type

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,14 +1,35 @@
+import type { DirectionalLight, AmbientLight } from 'three'
+
+interface ColorProperty {
+  getHexString: () => string
+  set: (hexString: string) => void
+}
+
+const createColorPropertyAdapter = (object: DirectionalLight | AmbientLight): ColorProperty => {
+  return {
+    getHexString: () => {
+      return object.color.getHexString()
+    },
+    set: (hexString: string) => {
+      object.color.setHex(parseInt(hexString.replace('#', ''), 16))
+    }
+  }
+}
+
 export class ColorGUIHelper {
-  constructor (object, prop) {
-    this.object = object
+  object: ColorProperty
+  prop: string
+
+  constructor (object: DirectionalLight | AmbientLight, prop: string) {
+    this.object = createColorPropertyAdapter(object)
     this.prop = prop
   }
 
   get value (): string {
-    return `#${this.object[this.prop].getHexString()}`
+    return `#${this.object.getHexString()}`
   }
 
-  set value (hexString) {
-    this.object[this.prop].set(hexString)
+  set value (hexString: string) {
+    this.object.set(hexString)
   }
 }
